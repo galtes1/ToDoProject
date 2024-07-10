5526640633f2811dd55ff35549adb2d561812e00
galtes1 <155002780+galtes1@users.noreply.github.com>
2024-07-10 18:55:59 +0300

    to do list, main board,

---
+++ TaskManagerService.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoProject
+{
+    class TaskManagerService
+    {
+
+        public List<TaskModel> tasks;
+
+        public TaskManagerService()
+        {
+            tasks = new List<TaskModel>();
+        }
+
+        public void AddTask(TaskModel task)
+        {
+            tasks.Add(task);
+        }
+        public void RemoveTask(int taskId) { }
+        public void UpdateTask(int taskId, string newDiscription) { }
+        public void ToggleIsComplete(int taskId) { }
+
+
+    }
+}
---
+++ TaskModel.cs
@@ -0,0 +1,24 @@
+﻿namespace ToDoProject
+{
+    class TaskModel
+    {
+        public int TaskId { get; set; }
+        public string TaskDiscription { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime Created { get; set; }
+
+        public TaskModel(int taskId, string taskDiscription)
+        {
+            TaskId = taskId;
+            TaskDiscription = taskDiscription;
+            IsCompleted = false;
+            Created = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{TaskId}. {TaskDiscription} - {Created.ToString()} - Done? {IsCompleted}";
+        }
+    }
+
+}
